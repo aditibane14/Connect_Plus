15f47bbdbabe2551437468869c13a71ac2edb056
Aditi <aditi.bane14@gmail.com>
2022-07-01 19:03:30 +0530

    Refresh not working

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+//import {uuid} from 'uuidv4';
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);   
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+    //setContacts([...contacts, {id: uuid(), ...contact }]);
+  };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log(retriveContacts);
+    //if (retriveContacts) setContacts(retriveContacts);
+    setContacts([...contacts, retriveContacts]);
+    console.log(contacts);
+  },[]);
+
+  useEffect(() => {
+    console.log(contacts);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
+    </div>
+);
+}
+
+export default App;
